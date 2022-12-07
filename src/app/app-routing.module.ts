177fd9af472e2ae94464234973890a40d877a3f9
kaustdixit <kaustdixit@deloitte.com>
2022-12-07 22:40:57 +0530

    Added header, declared other 3 page components, added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import { HomepageComponent } from './homepage/homepage.component';
+import { ListingpageComponent } from './listingpage/listingpage.component';
+import { ProductpageComponent } from './productpage/productpage.component';
+
+const routes: Routes = [
+  {path : "", component : HomepageComponent},
+  {path : "Home", component : HomepageComponent},
+  {path : "List", component : ListingpageComponent},
+  {path : "Product/:id", component : ProductpageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ListingpageComponent } from './listingpage/listingpage.component';
+import { ProductpageComponent } from './productpage/productpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomepageComponent,
+    ListingpageComponent,
+    ProductpageComponent
   ],
   imports: [
     BrowserModule,
