15af9946e61b83574aecaea17f3c8de9f4012583
kaustdixit <kaustdixit@deloitte.com>
2022-12-07 23:01:33 +0530

    Homepage added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ListingpageComponent } from './listingpage/listingpage.component';
 import { ProductpageComponent } from './productpage/productpage.component';
+import { CarouselComponent } from './homepage/carousel/carousel.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { ProductpageComponent } from './productpage/productpage.component';
     HeaderComponent,
     HomepageComponent,
     ListingpageComponent,
-    ProductpageComponent
+    ProductpageComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
