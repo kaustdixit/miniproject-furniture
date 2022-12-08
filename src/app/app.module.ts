28447161c17dfcf070fe1a5e6e324e422a820f0d
kaustdixit <kaustdixit@deloitte.com>
2022-12-08 16:08:43 +0530

    Folder structure updated and added mock data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { ListingpageComponent } from './listingpage/listingpage.component';
-import { ProductpageComponent } from './productpage/productpage.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { ListingpageComponent } from './components/listingpage/listingpage.component';
+import { ProductpageComponent } from './components/productpage/productpage.component';
+
 
 @NgModule({
   declarations: [
