45ac6a1d9e8f38cb88999f3a5b0736e773467a3e
kaustdixit <kaustdixit@deloitte.com>
2022-12-15 17:58:37 +0530

    Data loading with id based routing done on product page

--- src/app/components/productpage/productpage.component.ts
+++ src/app/components/productpage/productpage.component.ts
@@ -5,20 +5,19 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-productpage',
   templateUrl: './productpage.component.html',
-  styleUrls: ['./productpage.component.css']
+  styleUrls: ['./productpage.component.css'],
 })
 export class ProductpageComponent implements OnInit {
-  public productDataone: any;
-  oneProd:any = {};
-  constructor(private activatedRouteObj: ActivatedRoute){
-
-  }
+  public productDataone: any[] = [];
+  oneProd: any = {};
+  constructor(private activatedRouteObj: ActivatedRoute) {}
   ngOnInit(): void {
     this.productDataone = mockProductData;
 
-    let no = this.activatedRouteObj.snapshot.params["name"];
-    this.oneProd =  this.productDataone.find((product: { name: any; }) => product.name == no);
+    let no = this.activatedRouteObj.snapshot.params['id'];
+    this.oneProd = this.productDataone.find((item) => item._id.$oid === no);
+    console.log(no);
     console.log(this.oneProd);
+    console.log(this.productDataone);
   }
 }
-
