e74e95ea11059a6d65af840f68a4c54f9f5a1c59
kaustdixit <kaustdixit@deloitte.com>
2023-01-13 17:48:51 +0530

    Authentication guard on listing page using JWT token

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,13 @@ import { LoginComponent } from './components/header/login/login.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { ListingpageComponent } from './components/listingpage/listingpage.component';
 import { ProductpageComponent } from './components/productpage/productpage.component';
+import { AuthGuard } from './guards/auth.guard';
 
 
 const routes: Routes = [
   {path : "", component : HomepageComponent},
   {path : "Home", component : HomepageComponent},
-  {path : "List", component : ListingpageComponent},
+  {path : "List", component : ListingpageComponent, canActivate:[AuthGuard]},
   {path : "Product/:id", component : ProductpageComponent},
   {path : "login", component: LoginComponent}
 ];
